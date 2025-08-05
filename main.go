0180141c4e6404ea32087dfa4fc5049a5e11cd69
Almaz Fedorov <fedoro3valmaz@gmail.com>
2025-08-05 14:04:52 +0300

    4.8. Создание канала

--- main.go
+++ main.go
@@ -3,28 +3,19 @@ package main
 import (
 	"fmt"
 	"net/http"
-	"sync"
-	"time"
 )
 
 func main() {
-	t := time.Now()
-	var wg sync.WaitGroup
-	for i := 0; i < 10; i++ {
-		wg.Add(1)
-		go func() {
-			getHttpCode()
-			wg.Done()
-		}()
-	}
-	wg.Wait()
-	fmt.Println(time.Since(t))
+	code := make(chan int)
+	go getHttpCode(code)
+	<-code
 }
 
-func getHttpCode() {
+func getHttpCode(codeCh chan int) {
 	resp, err := http.Get("https://google.com")
 	if err != nil {
 		fmt.Printf("Ошибка %s", err.Error())
 	}
 	fmt.Printf("Код: %d\n", resp.StatusCode)
+	codeCh <- resp.StatusCode
 }
