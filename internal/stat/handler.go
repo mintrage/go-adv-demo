9bcfcb1ea8b3d66bf276d98fe0fa04003713cc22
Almaz Fedorov <fedoro3valmaz@gmail.com>
2025-08-13 16:08:06 +0300

    13.16. Group By в Gorm

--- internal/stat/handler.go
+++ internal/stat/handler.go
@@ -1,16 +1,16 @@
 package stat
 
 import (
-	"fmt"
 	"go/adv-demo/configs"
 	"go/adv-demo/pkg/middleware"
+	"go/adv-demo/pkg/res"
 	"net/http"
 	"time"
 )
 
 const (
-	FilterByDay   = "day"
-	FilterByMonth = "month"
+	GroupByDay   = "day"
+	GroupByMonth = "month"
 )
 
 type StatHandlerDeps struct {
@@ -32,21 +32,22 @@ func NewStatHandler(router *http.ServeMux, deps StatHandlerDeps) {
 
 func (h *StatHandler) GetStat() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
-		from, err := time.Parse("2006-01-01", r.URL.Query().Get("from"))
+		from, err := time.Parse("2006-01-02", r.URL.Query().Get("from"))
 		if err != nil {
 			http.Error(w, "Invalid from param", http.StatusBadRequest)
 			return
 		}
-		to, err := time.Parse("2006-01-01", r.URL.Query().Get("to"))
+		to, err := time.Parse("2006-01-02", r.URL.Query().Get("to"))
 		if err != nil {
 			http.Error(w, "Invalid to param", http.StatusBadRequest)
 			return
 		}
 		by := r.URL.Query().Get("by")
-		if by != FilterByDay && by != FilterByMonth {
+		if by != GroupByDay && by != GroupByMonth {
 			http.Error(w, "Invalid by param", http.StatusBadRequest)
 			return
 		}
-		fmt.Println(from, to, by)
+		stats := h.StatRepository.GetStats(by, from, to)
+		res.Json(w, stats, 200)
 	}
 }
