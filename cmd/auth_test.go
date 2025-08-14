4264263f8f14fe64f5101e7570ca67eca165f017
Almaz Fedorov <fedoro3valmaz@gmail.com>
2025-08-14 15:43:31 +0300

    14.4. Упражнение Отрицательный тест

--- cmd/auth_test.go
+++ cmd/auth_test.go
@@ -4,12 +4,43 @@ import (
 	"bytes"
 	"encoding/json"
 	"go/adv-demo/internal/auth"
+	"io"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 )
 
 func TestLoginSuccess(t *testing.T) {
+	ts := httptest.NewServer(App())
+	defer ts.Close()
+
+	data, _ := json.Marshal(&auth.LoginRequest{
+		Email:    "a2@a.ru",
+		Password: "1",
+	})
+
+	res, err := http.Post(ts.URL+"/auth/login", "application/json", bytes.NewReader(data))
+	if err != nil {
+		t.Fatal(err)
+	}
+	if res.StatusCode != 200 {
+		t.Fatalf("Expected %d got %d", 200, res.StatusCode)
+	}
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	var resData auth.LoginResponse
+	err = json.Unmarshal(body, &resData)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if resData.Token == "" {
+		t.Fatal("Token empty")
+	}
+}
+
+func TestLoginFail(t *testing.T) {
 	ts := httptest.NewServer(App())
 	defer ts.Close()
 
@@ -22,7 +53,7 @@ func TestLoginSuccess(t *testing.T) {
 	if err != nil {
 		t.Fatal(err)
 	}
-	if res.StatusCode != 200 {
-		t.Fatalf("Expected %d got %d", 200, res.StatusCode)
+	if res.StatusCode != 401 {
+		t.Fatalf("Expected %d got %d", 401, res.StatusCode)
 	}
 }
