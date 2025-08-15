d3a531fb00189646cee1578e765be6c07fea5592
Almaz Fedorov <fedoro3valmaz@gmail.com>
2025-08-15 17:33:31 +0300

    14.8. Unit тесты

---
+++ pkg/jwt/jwt_test.go
@@ -0,0 +1,24 @@
+package jwt_test
+
+import (
+	"go/adv-demo/pkg/jwt"
+	"testing"
+)
+
+func TestJWTCreate(t *testing.T) {
+	const email = "a@a.ru"
+	jwtService := jwt.NewJWT("6d0cba408179982ef5893fba1370c95e389480d94509fec1ca3c5f7e22b52fe1")
+	token, err := jwtService.Create(jwt.JWTData{
+		Email: email,
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+	isValid, data := jwtService.Parse(token)
+	if !isValid {
+		t.Fatal("Token is invalid")
+	}
+	if data.Email != email {
+		t.Fatalf("Email %s not equal %s", data.Email, email)
+	}
+}
